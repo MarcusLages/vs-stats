91b0a137060852113df40d6dcabab20227d31ae9
Ben Nguyen <benc.nguyen02@gmail.com>
2025-11-08 21:22:19 -0800

    Added way to track commits through saved text document calls

--- src/extension.js
+++ src/extension.js
@@ -1,18 +1,21 @@
 import * as vscode from "vscode";
+import { getCurRepo } from "./utils/git.js";
 import { MainWindow } from "./components/view_window.js";
 import { LineTracker } from "./tracker/lineTracker.js"
+import { CommitTracker } from "./tracker/getCommits.js";
 
 // activate(context: vscode.ExtensionContext)
 export function activate(context) {
 	console.log('Extension "statosaurus" activated.');
-
 	const mainWindow = vscode.window.registerTreeDataProvider(
 		'mainWindow',
 		new MainWindow()
+
 	)
 
-    // Example usage
-    new LineTracker().start({update: function(m) { console.log(`+${m.added} -${m.removed}`) }})
+    // Start tracking
+    new LineTracker().start({update: function(m) { console.log(`+${m.added} -${m.removed}`) }});
+    new CommitTracker().start();
 
 	context.subscriptions.push(mainWindow);
 }
